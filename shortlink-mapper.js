e9b15234770d6e81915e786b78ab2d9cff9459c0
Victor Khov <khovtyv@gmail.com>
2025-05-06 21:08:19 +0700

    added new guest to shortlink-mapper.js

--- shortlink-mapper.js
+++ shortlink-mapper.js
@@ -123,7 +123,7 @@ guestMap["g102"] = "Satra";
 guestMap["g103"] = "Bonita";
 guestMap["g104"] = "Ek Viraksunnry & Husband";
 guestMap["g105"] = "SinEk KunrathDevy";
-guestMap["106"] = "លោក ទុង រតនា";
+guestMap["g106"] = "លោក ទុង រតនា";
 
 
 // Function to look up guest by short code and redirect as needed
